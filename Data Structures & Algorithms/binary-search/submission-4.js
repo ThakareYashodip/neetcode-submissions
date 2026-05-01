a31c19758a20869955c50db6a028845e394b4e0f
ThakareYashodip <126881371+ThakareYashodip@users.noreply.github.com>
2026-05-01 23:13:31 +0530

    Add: binary-search - submission-4

---
+++ Data Structures & Algorithms/binary-search/submission-4.js
@@ -0,0 +1,22 @@
+class Solution {
+    search(nums, target) {
+        let l = 0;
+        let r = nums.length - 1; 
+
+        while (l <= r) {
+    
+            let m = Math.floor(l + (r - l) / 2);
+
+            if (nums[m] === target) {
+                return m;
+            }
+            
+            if (nums[m] > target) {
+                r = m - 1;
+            } else {
+                l = m + 1;
+            }
+        }
+        return -1;
+    }
+}
